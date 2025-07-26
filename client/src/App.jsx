8d2880621459a61724b549311cc556ed53776021
Randima Dilshani <randimadilshani98@gmail.com>
2025-07-26 17:08:55 +0530

    Frontend setup Implemented

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center p-6">
+      <h1 className="text-3xl font-bold">TrackZen Attendance System</h1>
+    </div>
+  );
+}
+
+
+export default App
